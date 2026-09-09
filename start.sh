864ac18f073c5a7db51448796c1c3a15710d0906
jnc260 <jennifer.colna@gmail.com>
2026-09-09 12:41:34 -0500

    fix start

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-  #!/bin/sh
-  set -e
+#!/bin/sh
+set -e
 
-  (cd pantry-agent/pantry-agent && node .mastra/output/index.mjs) &
-  cd api && node dist/main.js
+(cd pantry-agent/pantry-agent && PORT=4111 node .mastra/output/index.mjs) &
+cd api && node dist/main.js
